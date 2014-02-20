0f7e8792cc76f1a332bad0911fa2ece9848da11e
David Kraft <kraftd@goldmail.etsu.edu>
2014-02-19 20:36:16 -0500

    First Commit

---
+++ MyJournal/journalItem.h
@@ -0,0 +1,13 @@
+//
+//  journalItem.h
+//  MyJournal
+//
+//  Created by David Kraft on 2/19/14.
+//  Copyright (c) 2014 David Kraft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface journalItem : NSObject
+
+@end
---
+++ MyJournal/journalItem.m
@@ -0,0 +1,13 @@
+//
+//  journalItem.m
+//  MyJournal
+//
+//  Created by David Kraft on 2/19/14.
+//  Copyright (c) 2014 David Kraft. All rights reserved.
+//
+
+#import "journalItem.h"
+
+@implementation journalItem
+
+@end
